f8a8fc869e6a4074a581e4f8e331ef658a7cee9d
Nadi Adem <NAdem@spartaglobal.com>
2019-01-15 10:12:26 +0000

    Fourth upload with commit, the third upload was done without a commit

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 alert("Calculator!");
-
-
-
 var activeCal = true;
-
+var advanced = false;
+var advTemp = false;
 while (activeCal) {
     var firstNum = parseInt(prompt("Press your first number"));
     var symbol = prompt("Enter your opertator")
@@ -22,23 +20,36 @@ while (activeCal) {
         if (sign === "/") {
             return (num1 / num2);
         }
-
     }
     var sum = BasicCalculate(firstNum, secondNum, symbol);
     alert(sum);
-
     var choice = prompt("will you like to continue -- yes/no");
     if (choice === "yes") {
         activeCal = true;
         var sum = BasicCalculate(firstNum, secondNum, symbol);
         alert(sum);
     } else if (choice === "no") {
-       var other =  prompt("want to try my BMI calculator ? yes/no");
-        if(other === "yes")
-        {
+        var other = prompt("want to try my BMI calculator or temperature converter ? PRESS 1 or 2");
+        if (other === "1") {
             alert("BMI calculator in progress");
-        }else if (other === "no"){
+            advanced = true;
+            // Advanced calculator
+            while (advanced === true) {
+                var weight = prompt("Input your weight in KG please.");
+                var height = prompt("Input your height in meters please.");
+                function BMIcal(kg, metre2) {
+                    var total = kg / (metre2 * metre2);
+                    return total;
+                }
+                var Bmitotal = BMIcal(weight, height);
+                alert(Bmitotal);
+                break;
+            }
+        } else if (other === "2") {
             activeCal = false;
+            advTemp = true;
+            var sum = BasicCalculate(firstNum, secondNum, symbol);
+            alert(sum);
         }
     }
 }
